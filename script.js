910becf67df2877a5c7f660a13cc29d36aaf05ac
fh-fahad <fahadbin8900@gmail.com>
2024-03-02 21:07:21 +0600

    resume and design update

--- script.js
+++ script.js
@@ -1,32 +1,69 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        // sticky navbar on scroll script
-        { this.scrollY > 20 ? $('.navbar').addClass("sticky") : $('.navbar').removeClass("sticky") }
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      document.querySelector(".navbar").classList.add("sticky");
+    } else {
+      document.querySelector(".navbar").classList.remove("sticky");
+    }
 
-        // scroll-up button show/hide script
-        { this.scrollY > 200 ? $('.scroll-up-btn').addClass("show") : $('.scroll-up-btn').removeClass("show") }
+    // scroll-up button show/hide script
+    if (this.scrollY > 200) {
+      document.querySelector(".scroll-up-btn").classList.add("show");
+    } else {
+      document.querySelector(".scroll-up-btn").classList.remove("show");
+    }
+  });
+
+  document
+    .querySelector(".scroll-up-btn")
+    .addEventListener("click", function () {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+      // removing smooth scroll on slide-up button click
+      document.documentElement.style.scrollBehavior = "auto";
     });
 
-    $('.scroll-up-btn').click(function () {
-        $('html').animate({ scrollTop: 0 });
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
+  document.querySelectorAll(".navbar .menu li a").forEach(function (link) {
+    link.addEventListener("click", function () {
+      // applying smooth scroll on menu items click
+      document.documentElement.style.scrollBehavior = "smooth";
     });
+  });
 
-    $('.navbar .menu li a').click(function () {
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+  document.querySelector(".menu-btn").addEventListener("click", function () {
+    document.querySelector(".navbar .menu").classList.toggle("active");
+    document.querySelector(".menu-btn i").classList.toggle("active");
+  });
 
-    $('.menu-btn').click(function () {
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-
-    new Typed(".typing", {
-        strings: ["A MERN Stack Developer", "A Compititive Programmer", "A Web Designer and Developer", "An Android App Developer"],
-        typeSpeed: 150,
-        backSpeed: 80,
-        loop: true
-    });
+  new Typed(".typing", {
+    strings: [
+      "A Frontend Developer",
+      "A Backend Developer",
+      //"A MERN Stack Developer",
+      "A Competitive Programmer",
+    ],
+    typeSpeed: 150,
+    backSpeed: 80,
+    loop: true,
+  });
 });
+
+function showImage(element) {
+  var projectDescription = element.querySelector(".projectDescription");
+  var projectImage = element.querySelector(".projectImage");
+  var hideElement = element.querySelector(".hide");
+
+  projectDescription.style.display = "none";
+  projectImage.style.display = "block";
+  hideElement.style.display = "none";
+}
+
+function showParagraph(element) {
+  var projectDescription = element.querySelector(".projectDescription");
+  var projectImage = element.querySelector(".projectImage");
+  var hideElement = element.querySelector(".hide");
+
+  projectDescription.style.display = "block";
+  projectImage.style.display = "none";
+  hideElement.style.display = "block";
+}
