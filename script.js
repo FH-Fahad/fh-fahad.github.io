15437c213c8b19be9850101997911f3e8e7370ba
fh-fahad <fahadbin8900@gmail.com>
2024-03-03 13:50:47 +0600

    responsiveness added

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-// $(document).ready(function () {
-//   $(window).scroll(function () {
-//     // sticky navbar on scroll script
-//     {
-//       this.scrollY > 20
-//         ? $(".navbar").addClass("sticky")
-//         : $(".navbar").removeClass("sticky");
-//     }
-
-//     // scroll-up button show/hide script
-//     {
-//       this.scrollY > 200
-//         ? $(".scroll-up-btn").addClass("show")
-//         : $(".scroll-up-btn").removeClass("show");
-//     }
-//   });
-
-//   $(".scroll-up-btn").click(function () {
-//     $("html").animate({ scrollTop: 0 });
-//     // removing smooth scroll on slide-up button click
-//     $("html").css("scrollBehavior", "auto");
-//   });
-
-//   $(".navbar .menu li a").click(function () {
-//     // applying again smooth scroll on menu items click
-//     $("html").css("scrollBehavior", "smooth");
-//   });
-
-//   $(".menu-btn").click(function () {
-//     $(".navbar .menu").toggleClass("active");
-//     $(".menu-btn i").toggleClass("active");
-//   });
-
-// });
-
 $(document).ready(function () {
   $(window).scroll(function () {
     // sticky navbar on scroll script
